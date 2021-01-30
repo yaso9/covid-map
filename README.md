2c20f61c4f2b789586148d79db468d1e7ed3be88
Yahya Sohail <yahyasohail@hotmail.com>
2021-01-30 14:04:36 -0600

    Add covid case overlay to the map.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Covid Map
+
+## Sources
+- Covid data is sourced from the Covid-19 Statistics API (covid-api.com)
+- Data for county boudaries is sourced from https://eric.clst.org/tech/usgeojson/
+- Map color scheme was generated using Color Brewer (https://colorbrewer2.org/?type=sequential&scheme=Reds&n=8)
--- www/scripts/index.js
+++ www/scripts/index.js
@@ -1,7 +1,12 @@
-import "../style/style.scss";
-import axios from 'axios';
 import "leaflet";
+import "leaflet.vectorgrid";
 import "leaflet/dist/leaflet.css";
+
+import axios from "axios";
+import { DateTime } from "luxon";
+
+import "../style/style.scss";
+import countyData from "./countyBoundaries.json";
 
 document.addEventListener("DOMContentLoaded", () => {
   const map = L.map("map").setView([37.09024, -95.712891], 5);
@@ -9,35 +14,88 @@ document.addEventListener("DOMContentLoaded", () => {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(map);
+
+  (async () => {
+    const cases = await getCases(DateTime.local().minus({ days: 1 }));
+
+    // Add the case data to the GeoJSON
+    let maxConfirmed = 0;
+    for (const feature of countyData.features) {
+      const { confirmed, deaths } = cases[
+        parseInt(feature.properties.GEO_ID.split("US")[1])
+      ] || {
+        confirmed: 0,
+        deaths: 0,
+      };
+
+      if (!cases[parseInt(feature.properties.GEO_ID.split("US")[1])])
+        console.log(feature);
+
+      feature.properties.CONFIRMED = confirmed;
+      feature.properties.DEATHS = deaths;
+
+      if (confirmed > maxConfirmed) maxConfirmed = confirmed;
+    }
+    console.log(maxConfirmed);
+
+    const colors = [
+      "#fff5f0",
+      "#fee0d2",
+      "#fcbba1",
+      "#fc9272",
+      "#fb6a4a",
+      "#ef3b2c",
+      "#cb181d",
+      "#99000d",
+    ].reverse();
+    function getColor(pop) {
+      return pop > 100000
+        ? colors[7]
+        : pop > 50000
+        ? colors[6]
+        : pop > 10000
+        ? colors[5]
+        : pop > 5000
+        ? colors[4]
+        : pop > 1000
+        ? colors[3]
+        : pop > 500
+        ? colors[2]
+        : pop > 100
+        ? colors[1]
+        : colors[0];
+    }
+
+    L.vectorGrid
+      .slicer(countyData, {
+        vectorTileLayerStyles: {
+          sliced: (properties) => {
+            return {
+              stroke: false,
+              fillColor: getColor(properties.CONFIRMED),
+              fillOpacity: 1,
+              fill: true,
+            };
+          },
+        },
+      })
+      .addTo(map);
+  })();
 });
 
-getCases("06/30/2019")
-function getCases(date){
-    var today = new Date();
-    // Source: https://stackoverflow.com/questions/1531093/how-do-i-get-the-current-date-in-javascript
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = today.getFullYear();
+async function getCases(date) {
+  const API_URL = "https://covid-api.com/api/reports?iso=USA";
 
-    today = new Date(mm + '/' + dd + '/' + yyyy);
-    console.log(today);
-    // source: https://www.geeksforgeeks.org/how-to-calculate-the-number-of-days-between-two-dates-in-javascript/
-    var date1 = new Date(date); 
-
-    var Difference_In_Time = today.getTime() - date1.getTime(); 
-    var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24); 
-    console.log(Difference_In_Days);
-
-
-    axios.get("https://disease.sh/v3/covid-19/nyt/states/Alabama%2C%20Alaska%2C%20American%20Samoa%2C%20Arizona%2C%20Arkansas%2C%20California%2C%20Colorado%2C%20Connecticut%2C%20Delaware%2C%20District%20of%20Columbia%2C%20Florida%2C%20Georgia%2C%20Guam%2C%20Hawaii%2C%20Idaho%2C%20Illinois%2C%20Indiana%2C%20Iowa%2C%20Kansas%2C%20Kentucky%2C%20Louisiana%2C%20Maine%2C%20Maryland%2C%20Massachusetts%2C%20Michigan%2C%20Minnesota%2C%20Minor%20Outlying%20Islands%2C%20Mississippi%2C%20Missouri%2C%20Montana%2C%20Nebraska%2C%20Nevada%2C%20New%20Hampshire%2C%20New%20Jersey%2C%20New%20Mexico%2C%20New%20York%2C%20North%20Carolina%2C%20North%20Dakota%2C%20Northern%20Mariana%20Islands%2C%20Ohio%2C%20Oklahoma%2C%20Oregon%2C%20Pennsylvania%2C%20Puerto%20Rico%2C%20Rhode%20Island%2C%20South%20Carolina%2C%20South%20Dakota%2C%20Tennessee%2C%20Texas%2C%20U.S.%20Virgin%20Islands%2C%20Utah%2C%20Vermont%2C%20Virginia%2C%20Washington%2C%20West%20Virginia%2C%20Wisconsin%2C%20Wyoming?lastdays=0")
-.then(response =>{
-    for(let states in response)
-    {
-        console.log(response.data);
+  const counties = {};
+  const data = (await axios.get(`${API_URL}&date=${date.toFormat("y-LL-dd")}`))
+    .data.data;
+  for (const { region } of data) {
+    for (const county of region.cities) {
+      counties[county.fips] = {
+        confirmed: county.confirmed,
+        deaths: county.deaths,
+      };
     }
-    
-})
-
+  }
+  return counties;
 }
-
-
